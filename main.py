1558c805d27b3bbb9249b3619ce6e5db1e481807
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-04-27 17:01:46 -0400

    added exe file and made minor change to program

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import webbrowser
 import time
 import random
 
-while True:
+start_time = time.time()    #save current time
+while (time.time()-start_time)<15:      #run for 15 seconds
     visit = random.choice(['google.com', 'youtube.com'])
     site = "http://{}".format(visit)
     webbrowser.open(site)
     
-    breaktime = random.randrange(5,10)
-    time.sleep(breaktime)
+#    breaktime = random.randrange(5,40)
+    time.sleep(5)   #sleep for 5 seconds, can be replaced with 'breaktime' to make it random
